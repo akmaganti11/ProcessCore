bf1cbe7447994df009367973ad4f7338f5692db6
akmaganti11 <akmaganti04@gmail.com>
2023-02-26 23:44:51 +0530

    initial commit

---
+++ src/main/java/core/process/ProcessCoreApplication.java
@@ -0,0 +1,13 @@
+package core.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/process/ProcessCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package core.process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
